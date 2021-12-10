c4093ce8568d41f02ca74d75e339c9a771eecd69
Alessio Proietti <alessio.proietti@artwaresoftware.com>
2021-12-10 18:11:46 +0100

    cicd

--- setup.py
+++ setup.py
@@ -1,11 +1,26 @@
-from setuptools import setup
+import setuptools
 
-setup(name='compyte_kernel',
-      version='0.0.1',
-      description='compute kernel',
-      url='https://github.com/automacoin/compyte-kernel',
-      author='AutomaCoin',
-      author_email='alessio.proietti@protonmail.com',
-      license='Apache License Version 2.0',
-      packages=['compyte-kernel'],
-      zip_safe=False)
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="compyte-kernel",
+    version="0.0.1-alpha",
+    author="AutomaCoin Devs",
+    author_email="alessio.proietti@protonmail.com",
+    description="A compute kernel",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/automacoin/compyte-kernel",
+    project_urls={
+        "Bug Tracker": "https://github.com/automacoin/compyte-kernel/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
