fd8935ee941bcc38748b2b9f1823868a2e17be58
Alessio Proietti <alessio.proietti@artwaresoftware.com>
2021-12-10 17:44:19 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='compyte_kernel',
+      version='0.0.1',
+      description='compute kernel',
+      url='https://github.com/automacoin/compyte-kernel',
+      author='AutomaCoin',
+      author_email='alessio.proietti@protonmail.com',
+      license='Apache License Version 2.0',
+      packages=['compyte-kernel'],
+      zip_safe=False)
